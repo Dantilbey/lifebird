432d9bf02b12573ed605d7c59738a78c647d6890
Daniel Watts <silwly@gmail.com>
2014-02-10 13:58:52 +0000

    Changes to reflect new registration script

--- examples/pdo/create_user_table.php
+++ examples/pdo/create_user_table.php
@@ -4,25 +4,25 @@ $table = 'users';
 try {
 	$db = new PDO("mysql:dbname=lifebird;host=localhost", "root", "");
 	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ); #error handling
-	$sql = "CREATE TABLE Users 
-               (
-                id INT(11) NOT NULL auto_increment,
-                username VARCHAR(16) NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                gender ENUM('m','f') NOT NULL,
-                website VARCHAR(255) NULL,
-                country VARCHAR(255) NULL,
-                replevel ENUM('a','b','c','d') NOT NULL DEFAULT 'a',
-                avatar VARCHAR(255) NULL,
-                ip VARCHAR(255) NOT NULL,
-                signup DATETIME NOT NULL,
-                lastlogin DATETIME NOT NULL,
-                notescheck DATETIME NOT NULL,
-                activated ENUM('0','1') NOT NULL DEFAULT '0',
-                PRIMARY KEY (id),
-                UNIQUE KEY username (username, email)
-                )"; # create users table
+  $sql = "CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_code` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `gender` enum('m','f') NOT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `replevel` enum('a','b','c','d') NOT NULL DEFAULT 'a',
+  `avatar` varchar(255) DEFAULT NULL,
+  `ip` varchar(255) NOT NULL,
+  `signup` datetime NOT NULL,
+  `lastlogin` datetime NOT NULL,
+  `notescheck` datetime NOT NULL,
+  `activated` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;"
 	$db->exec($sql);
 	print("Created $table Table.\n");
 } catch (PDOException $e) {
